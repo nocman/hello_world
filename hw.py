9a18d6db5511cf1ea2b44998f84af80eb9cce53c
Robert Rędziak <robert.redziak@gmail.com>
2017-02-27 18:37:37 +0100

    Inicjalny commit.

---
+++ hw.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+def hello():
+  '''Hello-jello world. Nothing to see here, move along!'''
+
+  hellostring = 'Hello' + ' ' + 'world'
+  print("%s" % hellostring)
+
+if __name__ == '__main__':
+  hello()
