5aece4e49c01633c4ad7c9c12916651e516e38c2
Robert Rędziak <robert.redziak@gmail.com>
2017-02-27 22:26:29 +0100

    Wariantowość do przetestowania.

--- hw.py
+++ hw.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python
-def hello():
+def hello(who=None):
   '''Hello-jello world. Nothing to see here, move along!'''
 
-  hellostring = 'Hello' + ' ' + 'world!'
+  if who is not None:
+    hellostring = 'Hello ' + str(who) + '!'
+  else:
+    hellostring = 'Hello world!'
+
   print("%s" % hellostring)
 
 if __name__ == '__main__':
